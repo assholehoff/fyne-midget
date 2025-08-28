e821f3799b38ff4bd43925f1cd1281335b27082c
Anton Dahlén <39925638+assholehoff@users.noreply.github.com>
2025-08-28 15:19:12 +0200

    add New functions for Entry

--- entry.go
+++ entry.go
@@ -9,6 +9,16 @@ type Entry struct {
 	xwidget.CompletionEntry
 }
 
+func NewEntry() *Entry {
+	return NewEntryWithCompletions([]string{})
+}
+func NewEntryWithCompletions(options []string) *Entry {
+	e := &Entry{}
+	e.Options = options
+	e.ExtendBaseWidget(e)
+	return e
+}
+
 func (e *Entry) FocusGained() {
 	e.CompletionEntry.FocusGained()
 	e.TypedShortcut(&fyne.ShortcutSelectAll{})
